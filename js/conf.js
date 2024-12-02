5920c4ffec4f37109af6046dc3118456e8c7a69f
Developer-Chandan-Dev <chandan.dev.pro@gmail.com>
2024-12-02 06:40:46 +0530

    Home Page content updated

--- js/conf.js
+++ js/conf.js
@@ -1,4 +1,4 @@
-const PORT = `https://disco-tailors.netlify.app/`;
+const PORT = `http://127.0.0.1:5500/`;
 
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
